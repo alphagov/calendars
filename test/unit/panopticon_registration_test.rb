5563256feceda35808dde889dfee0521fc64dfc4
James Stewart <jys@ketlai.co.uk>
2012-07-04 22:22:26 +0100

    Add RegisterableCalendar class
    To ease panopticon registration we have a new wrapper class that opens up a JSON file and presents it in a form suitable for handing to the api adapter.

---
+++ test/unit/panopticon_registration_test.rb
@@ -0,0 +1,24 @@
+require_relative '../test_helper'
+
+require 'gds_api/panopticon'
+require 'registerable_calendar'
+
+class PanopticonRegistrationTest < ActiveSupport::TestCase
+
+  context "Panopticon registration" do
+
+    should "translate to Panopticon artefacts" do
+      registerer = GdsApi::Panopticon::Registerer.new(owning_app: "calendars")
+      file_path = Dir.glob(Rails.root.join("lib/data/*.json")).first
+
+      calendar = RegisterableCalendar.new(file_path)
+      artefact = registerer.record_to_artefact(calendar)
+
+      [:name, :description, :slug].each do |key|
+        assert artefact.has_key?(key), "Missing attribute: #{key}"
+      end
+
+      assert artefact[:live], "Is not live"
+    end
+  end
+end
