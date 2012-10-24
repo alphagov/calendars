f7ddc1680b2865bec7290cda057bc3127db701b2
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-10-24 16:08:13 +0000

    Update ical tests to require \r\n line breaks
    This is as per the spec (rfc2445 Section 4.1)

--- test/integration/icalendar_test.rb
+++ test/integration/icalendar_test.rb
@@ -6,31 +6,31 @@ class IcalendarTest < ActionDispatch::IntegrationTest
     should "contain all calendar events with an individual calendar" do
       get "/bank-holidays/england-and-wales-2011.ics"
 
-      output = "BEGIN:VCALENDAR\nPRODID;X-RICAL-TZSOURCE=TZINFO:-//com.denhaven2/NONSGML ri_cal gem//EN\nCALSCALE:GREGORIAN\nVERSION:2.0\n"
+      expected = "BEGIN:VCALENDAR\r\nPRODID;X-RICAL-TZSOURCE=TZINFO:-//com.denhaven2/NONSGML ri_cal gem//EN\r\nCALSCALE:GREGORIAN\r\nVERSION:2.0\r\n"
 
       repository = Calendar::Repository.new("bank-holidays")
       repository.find_by_division_and_year('england-and-wales','2011').events.each do |event|
-        output += "BEGIN:VEVENT\nDTEND;VALUE=DATE:#{event.date.strftime('%Y%m%d')}\nDTSTART;VALUE=DATE:#{event.date.strftime('%Y%m%d')}\nSUMMARY:#{event.title}\nEND:VEVENT\n"
+        expected << "BEGIN:VEVENT\r\nDTEND;VALUE=DATE:#{event.date.strftime('%Y%m%d')}\r\nDTSTART;VALUE=DATE:#{event.date.strftime('%Y%m%d')}\r\nSUMMARY:#{event.title}\r\nEND:VEVENT\r\n"
       end
 
-      output += "END:VCALENDAR\n"
+      expected << "END:VCALENDAR\r\n"
 
-      assert_equal output, @response.body
+      assert_equal expected, @response.body
     end
 
     should "contain all calendar events for combined calendars" do
       get "/bank-holidays/england-and-wales.ics"
 
-      output = "BEGIN:VCALENDAR\nPRODID;X-RICAL-TZSOURCE=TZINFO:-//com.denhaven2/NONSGML ri_cal gem//EN\nCALSCALE:GREGORIAN\nVERSION:2.0\n"
+      expected = "BEGIN:VCALENDAR\r\nPRODID;X-RICAL-TZSOURCE=TZINFO:-//com.denhaven2/NONSGML ri_cal gem//EN\r\nCALSCALE:GREGORIAN\r\nVERSION:2.0\r\n"
 
       repository = Calendar::Repository.new("bank-holidays")
       Calendar.combine(repository.all_grouped_by_division, 'england-and-wales').events.each do |event|
-        output += "BEGIN:VEVENT\nDTEND;VALUE=DATE:#{event.date.strftime('%Y%m%d')}\nDTSTART;VALUE=DATE:#{event.date.strftime('%Y%m%d')}\nSUMMARY:#{event.title}\nEND:VEVENT\n"
+        expected << "BEGIN:VEVENT\r\nDTEND;VALUE=DATE:#{event.date.strftime('%Y%m%d')}\r\nDTSTART;VALUE=DATE:#{event.date.strftime('%Y%m%d')}\r\nSUMMARY:#{event.title}\r\nEND:VEVENT\r\n"
       end
 
-      output += "END:VCALENDAR\n"
+      expected << "END:VCALENDAR\r\n"
 
-      assert_equal output, @response.body
+      assert_equal expected, @response.body
      end
   end
 end
