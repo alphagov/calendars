78757d71d5f93232a4d93813864f9e479fd16831
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-10-25 11:53:24 +0100

    Fix simplecov setup.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,16 @@
+require 'simplecov'
+require 'simplecov-rcov'
+
+SimpleCov.start 'rails'
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 require 'mocha'
-require 'simplecov'
-require 'simplecov-rcov'
 require 'slimmer/skin'
 require 'slimmer/test'
-
-SimpleCov.start 'rails'
-SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
 
 require 'webmock/test_unit'
 WebMock.disable_net_connect!(:allow_localhost => true)
