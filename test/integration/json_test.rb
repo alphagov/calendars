28885309a78c24ffe1544c902fc56da514bc48be
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-01-02 15:59:27 +0000

    Remove the per-year json and ics views
    These are no longer used by the bank holidays page, and aren't actually very useful to anyone.

--- test/integration/json_test.rb
+++ test/integration/json_test.rb
@@ -3,28 +3,6 @@ require_relative '../integration_test_helper'
 
 class JSONTest < ActionDispatch::IntegrationTest
   context "GET /calendars/<calendar>.json" do
-    should "contain calendar data with individual calendar" do
-      get "/bank-holidays/england-and-wales-2012.json"
-
-      expected = {
-        "events" => [
-          {"date"=>"2012-01-02", "notes"=>"Substitute day", "title"=>"New Year’s Day", "bunting"=>"true"},
-          {"date"=>"2012-06-04", "notes"=>"Substitute day", "title"=>"Spring bank holiday", "bunting"=>"true"},
-          {"date"=>"2012-06-05", "notes"=>"Extra bank holiday", "title"=>"Queen’s Diamond Jubilee", "bunting"=>"true"},
-          {"date"=>"2012-08-27", "notes"=>"", "title"=>"Summer bank holiday", "bunting"=>"true"},
-          {"date"=>"2012-12-25", "notes"=>"", "title"=>"Christmas Day", "bunting"=>"true"},
-          {"date"=>"2012-12-26", "notes"=>"", "title"=>"Boxing Day", "bunting"=>"true"},
-        ],
-        "division"=>"england-and-wales",
-        "year"=>"2012"
-      }
-
-      actual = JSON.parse(@response.body)
-      assert_equal expected["events"], actual["events"]
-      assert_equal expected["division"], actual["division"]
-      assert_equal expected["year"], actual["year"]
-    end
-
     should "contain calendar with division" do
       get "/bank-holidays/england-and-wales.json"
 
@@ -119,18 +97,10 @@ class JSONTest < ActionDispatch::IntegrationTest
       assert_equal expected, JSON.parse(@response.body)
     end
 
-    should "have redirects for old 'ni' division" do
+    should "have redirect for old 'ni' division" do
       get "/bank-holidays/ni.json"
       assert_equal 301, response.status
       assert_equal "http://www.example.com/bank-holidays/northern-ireland.json", response.location
-
-      get "/bank-holidays/ni-2012.json"
-      assert_equal 301, response.status
-      assert_equal "http://www.example.com/bank-holidays/northern-ireland-2012.json", response.location
-
-      get "/bank-holidays/ni-2013.json"
-      assert_equal 301, response.status
-      assert_equal "http://www.example.com/bank-holidays/northern-ireland-2013.json", response.location
     end
   end
 end
