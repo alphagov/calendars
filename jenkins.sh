901fe0001d2d9210e91b4dbabc449d6d45380e51
Mazz Mosley <mazzmosley@GDSMazz.local>
2013-01-14 17:15:37 +0000

    Set appropriate env variables

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
 
 export RAILS_ENV=test
+export GOVUK_APP_DOMAIN=dev.gov.uk
+export GOVUK_ASSET_HOST=http://static.dev.gov.uk
 
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 
