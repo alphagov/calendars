db584b55962b1901ec3a38750d22d6d85b554a88
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-03-08 17:12:46 +0000

    Bump plek to 1.3.1
    Avoid having to use govuk_setenv in dev.

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -x
 
 export RAILS_ENV=test
-export GOVUK_APP_DOMAIN=dev.gov.uk
-export GOVUK_ASSET_HOST=http://static.dev.gov.uk
 
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 
