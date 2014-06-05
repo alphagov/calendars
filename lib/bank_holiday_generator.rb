ba8c295b1a214dd8930b49707986ad93eb14a1a9
Camille Baldock <camille@camillebaldock.co.uk>
2014-06-05 22:16:17 +0100

    Fix incorrect output with double bank holidays

--- lib/bank_holiday_generator.rb
+++ lib/bank_holiday_generator.rb
@@ -10,68 +10,74 @@
 
 class BankHolidayGenerator
 
-  def initialize(year, nation)
+  def initialize(year, nation, specify_proclamation=false)
     @year = year
     @nation = nation
+    @specify_proclamation = specify_proclamation
     @bank_holidays = []
   end
 
   BANK_HOLIDAYS = {
-    "england-and-wales" => [
-      :new_years_day, #by proclamation
-      :good_friday,   #by proclamation
-      :easter_monday, #by proclamation
-      :early_may,     #by proclamation
-      :spring,
-      :last_monday_august,
-      :christmas,
-      :boxing_day,
-    ],
+    "england-and-wales" => {
+      :new_years_day      => true,
+      :good_friday        => true,
+      :easter_monday      => true,
+      :early_may          => true,
+      :spring             => false,
+      :last_monday_august => false,
+      :christmas          => false,
+      :boxing_day         => false,
+    },
 
-    "scotland" => [
-      :new_years_day_second_january_off,
-      :second_january,
-      :good_friday,
-      :early_may,
-      :spring,        #by proclamation
-      :first_monday_august,
-      :st_andrews,
-      :christmas,
-      :boxing_day,    #by proclamation
-    ],
+    "scotland" => {
+      :new_years_day_second_january_off => false,
+      :second_january                   => false,
+      :good_friday                      => false,
+      :early_may                        => false,
+      :spring                           => true,
+      :first_monday_august              => false,
+      :st_andrews                       => false,
+      :christmas                        => false,
+      :boxing_day                       => true,
+    },
 
-    "northern-ireland" => [
-      :new_years_day, #by proclamation
-      :st_patricks,
-      :good_friday,   #by proclamation
-      :easter_monday,
-      :early_may,     #by proclamation
-      :spring,
-      :battle_boyne,  #by proclamation
-      :last_monday_august,
-      :christmas,
-      :boxing_day,
-    ]
+    "northern-ireland" => {
+      :new_years_day      => true,
+      :st_patricks        => false,
+      :good_friday        => true,
+      :easter_monday      => false,
+      :early_may          => true,
+      :spring             => false,
+      :battle_boyne       => true,
+      :last_monday_august => false,
+      :christmas          => false,
+      :boxing_day         => false,
+    }
   }
 
-  attr_reader :year, :bank_holidays, :nation
+  attr_reader :year, :bank_holidays, :nation, :specify_proclamation
 
   def perform
-    BANK_HOLIDAYS.fetch(nation).each do |bank_holiday|
-      send(bank_holiday)
+    BANK_HOLIDAYS.fetch(nation).each do |bank_holiday_name, by_proclamation|
+      send(bank_holiday_name, by_proclamation)
     end
     bank_holidays.sort_by{|bh_hash| DateTime.parse(bh_hash.fetch("date"))}
   end
 
 private
 
-  def add_bank_holiday(title, date, substitute=false, bunting=true)
+  def add_bank_holiday(title, date, by_proclamation, substitute=false, bunting=true)
     bank_holiday_hash = {
-        "title"   => title,
-        "date"    => date.strftime("%d/%m/%Y"),
-        "notes"   => "",
-        "bunting" => bunting,
+        "title"           => title,
+        "date"            => date.strftime("%d/%m/%Y"),
+        "notes"           => "",
+        "bunting"         => bunting,
     }
+    if specify_proclamation
+      bank_holiday_hash.merge!(
+          { "by_proclamation" => by_proclamation }
+      )
+    end
     if substitute
       bank_holiday_hash.merge!(
         { "notes" => "common.substitute_day" }
@@ -81,84 +87,87 @@ private
   end
 
 
-  def new_years_day(second_january_off = false)
+  def new_years_day(by_proclamation, second_january_off = false)
     date = Date.new(year, 1, 1)
-    if second_january_off
+    if second_january_off && date.saturday?
+      new_date = date + 3
+      by_proclamation = true
+    elsif second_january_off
       new_date = substitute_day_next_day_off(date)
     else
       new_date = substitute_day(date)
     end
-    add_bank_holiday("bank_holidays.new_year", date, new_date != date)
+    add_bank_holiday("bank_holidays.new_year", new_date, by_proclamation, new_date != date)
   end
 
-  def new_years_day_second_january_off
-    new_years_day(true)
+  def new_years_day_second_january_off(by_proclamation)
+    new_years_day(by_proclamation, true)
   end
 
-  def second_january
+  def second_january(by_proclamation)
     date = Date.new(year, 1, 2)
     new_date = substitute_day(date)
-    add_bank_holiday("bank_holidays.2nd_january", new_date, new_date != date)
+    add_bank_holiday("bank_holidays.2nd_january", new_date, by_proclamation, new_date != date)
   end
 
-  def st_patricks
+  def st_patricks(by_proclamation)
     date = Date.new(year, 3, 17)
     new_date = substitute_day(date)
-    add_bank_holiday("bank_holidays.st_patrick", new_date, new_date != date)
+    add_bank_holiday("bank_holidays.st_patrick", new_date, by_proclamation, new_date != date)
   end
 
-  def good_friday
+  def good_friday(by_proclamation)
     date = easter-2
-    add_bank_holiday("bank_holidays.good_friday", date, false, false)
+    add_bank_holiday("bank_holidays.good_friday", date, by_proclamation, false, false)
   end
 
-  def easter_monday
+  def easter_monday(by_proclamation)
     date = easter+1
-    add_bank_holiday("bank_holidays.easter_monday", date)
+    add_bank_holiday("bank_holidays.easter_monday", date, by_proclamation)
   end
 
-  def early_may
+  def early_may(by_proclamation)
     date = first_monday_of_month(year, 5)
-    add_bank_holiday("bank_holidays.early_may", date)
+    add_bank_holiday("bank_holidays.early_may", date, by_proclamation)
   end
 
-  def spring
+  def spring(by_proclamation)
     date = last_monday_of_month(year, 5)
-    add_bank_holiday("bank_holidays.spring", date)
+    add_bank_holiday("bank_holidays.spring", date, by_proclamation)
   end
 
-  def battle_boyne
+  def battle_boyne(by_proclamation)
     date = Date.new(year, 7, 12)
     new_date = substitute_day(date)
-    add_bank_holiday("bank_holidays.battle_boyne", new_date, new_date != date, false)
+    add_bank_holiday("bank_holidays.battle_boyne", new_date, by_proclamation, new_date != date, false)
   end
 
-  def first_monday_august
+  def first_monday_august(by_proclamation)
     date = first_monday_of_month(year, 8)
-    add_bank_holiday("bank_holidays.summer", date)
+    add_bank_holiday("bank_holidays.summer", date, by_proclamation)
   end
 
-  def last_monday_august
+  def last_monday_august(by_proclamation)
     date = last_monday_of_month(year, 8)
-    add_bank_holiday("bank_holidays.late_august", date)
+    add_bank_holiday("bank_holidays.late_august", date, by_proclamation)
   end
 
-  def st_andrews
+  def st_andrews(by_proclamation)
     date = Date.new(year, 11, 30)
     new_date = substitute_day(date)
-    add_bank_holiday("bank_holidays.st_andrew", new_date, new_date != date)
+    add_bank_holiday("bank_holidays.st_andrew", new_date, by_proclamation, new_date != date)
   end
 
-  def christmas
+  def christmas(by_proclamation)
     date = Date.new(year, 12, 25)
     new_date = substitute_day_next_day_off(date)
-    add_bank_holiday("bank_holidays.christmas", new_date, new_date != date)
+    add_bank_holiday("bank_holidays.christmas", new_date, by_proclamation, new_date != date)
   end
 
-  def boxing_day
+  def boxing_day(by_proclamation)
     date = Date.new(year, 12, 26)
     new_date = substitute_day(date)
-    add_bank_holiday("bank_holidays.boxing_day", new_date, new_date != date)
+    add_bank_holiday("bank_holidays.boxing_day", new_date, by_proclamation, new_date != date)
   end
 
   #Date utilities
--- lib/tasks/bank_holidays.rake
+++ lib/tasks/bank_holidays.rake
@@ -15,4 +15,19 @@ namespace :bank_holidays do
       p "Please enter the year for which you want to generate bank holidays for"
     end
   end
+
+  desc "Generate JSON for UK bank holidays"
+  task :generate_json_with_proclamation, :year do |t, args|
+    year = args[:year].to_i
+    if year != 0
+      nations = ["england-and-wales", "scotland", "northern-ireland"]
+      nations.each do |nation|
+        generator = BankHolidayGenerator.new(year, nation, true)
+        bank_holidays = generator.perform
+        File.write("bank_holidays_#{year}_#{nation}.json", bank_holidays.to_json)
+      end
+    else
+      p "Please enter the year for which you want to generate bank holidays for"
+    end
+  end
 end
