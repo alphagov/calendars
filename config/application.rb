429b50b0d34494e83ad192cb93a5eb3f583babb6
tijmen <tijmen@gmail.com>
2015-09-24 08:43:16 +0100

    Move `Services` class to `/lib`
    The Services class shouldn't hold application specific code and is
    better at home in `/lib`. Other applications use this pattern as well.

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module Calendars
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
+    config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += %W(#{config.root}/app/services)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
