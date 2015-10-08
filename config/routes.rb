236199d77d2548ec4478fb8c57cd3ecb0dd01a71
David Basalla <davidbasalla@gmail.com>
2015-10-08 16:12:25 +0100

    Update routes to use get instead of match

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Calendars::Application.routes.draw do
   # Redirect for old 'ni' division slug in bank-holidays
   constraints(:format => /(json|ics)/) do
-    match '/bank-holidays/ni', :to => redirect("/bank-holidays/northern-ireland.%{format}")
+    get '/bank-holidays/ni', :to => redirect("/bank-holidays/northern-ireland.%{format}")
   end
 
-  match '/gwyliau-banc', :to => 'calendar#calendar', :defaults => { :scope => "gwyliau-banc", :locale => :cy }
-  match '/gwyliau-banc/:division', :to => 'calendar#division', :defaults => { :scope => "gwyliau-banc", :locale => :cy }
+  get '/gwyliau-banc', :to => 'calendar#calendar', :defaults => { :scope => "gwyliau-banc", :locale => :cy }
+  get '/gwyliau-banc/:division', :to => 'calendar#division', :defaults => { :scope => "gwyliau-banc", :locale => :cy }
 
-  match '/:scope', :to => 'calendar#calendar', :as => :calendar
-  match '/:scope/:division', :to => 'calendar#division', :as => :division
+  get '/:scope', :to => 'calendar#calendar', :as => :calendar
+  get '/:scope/:division', :to => 'calendar#division', :as => :division
 end
