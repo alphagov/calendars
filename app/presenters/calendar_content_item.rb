0327db0dce85ac2546eb572795771e6c8f13c5a0
Stephen Richards <stephen@stephenrichards.eu>
2015-12-02 12:07:20 +0000

    Use V2 of publishing-api to write calendars to the content store
    All apps should now use v2 of the publishing api to write content to the content-store. This change converts calendars to use v2 of the api. The differences are:
    
    - put content_item is replaced by two methods - put_content, and publish
    - the content_id is used as the key, rather than the base path
    - the base path is included in the payload

--- app/presenters/calendar_content_item.rb
+++ app/presenters/calendar_content_item.rb
@@ -10,14 +10,23 @@ class CalendarContentItem
     '/' + calendar.slug
   end
 
+  def update_type
+    'minor'
+  end
+
+  def content_id
+    calendar.content_id
+  end
+
   def payload
     {
       title: calendar.title,
-      content_id: calendar.content_id,
+      base_path: base_path,
+      content_id: content_id,
       format: 'placeholder_calendar',
       publishing_app: 'calendars',
       rendering_app: 'calendars',
-      update_type: 'minor',
+      update_type: update_type,
       locale: 'en',
       public_updated_at: Time.current.to_datetime.rfc3339,
       routes: [
