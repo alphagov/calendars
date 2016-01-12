c8df790d18ec7d2de874988c4488f4321b5853ac
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2016-01-12 13:59:37 +0000

    Add bearer_token for publishing API authentication

--- lib/services.rb
+++ lib/services.rb
@@ -2,6 +2,9 @@ require 'gds_api/publishing_api_v2'
 
 module Services
   def self.publishing_api
-    @publishing_api ||= GdsApi::PublishingApiV2.new(Plek.new.find('publishing-api'))
+    @publishing_api ||= GdsApi::PublishingApiV2.new(
+      Plek.new.find('publishing-api'),
+      bearer_token: ENV['PUBLISHING_API_BEARER_TOKEN'] || 'example'
+    )
   end
 end
