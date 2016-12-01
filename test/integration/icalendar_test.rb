708935ef19dbf26b217bc76c8d9013fb87fe50ab
David Silva <david.silva@digital.cabinet-office.gov.uk>
2016-12-01 10:14:37 +0000

    Set the division param with the apropriate slug
    This commit aims to discard the usage of I18n to build the translation key for a specific "division",
    the current implementation was hard to understand and maintain. This commit aims to keep it simple by using a hash that fetches the correct value from `params[:division]`
    
    For reference, this was introduced during [this
    commit](https://github.com/alphagov/calendars/commit/9a572aa8c5811a8f9b527101b9730af5f554cb0e#diff-81b9c07cfbeaf5d3fd88f6f12103bb77R58)
    
    I've also removed duplication in the test and made use of the I18n translation keys.

--- test/integration/icalendar_test.rb
+++ test/integration/icalendar_test.rb
@@ -8,34 +8,64 @@ class IcalendarTest < ActionDispatch::IntegrationTest
       File.stubs(:mtime).with(Rails.root.join("REVISION")).returns(Time.parse("2012-10-17 01:00:00"))
     end
 
-    should "contain all events in the given division" do
-      path = "/bank-holidays/england-and-wales.ics"
-      get path
+    calendars = {
+      "welsh" => "/gwyliau-banc/cymru-a-lloegr.ics",
+      "welsh-scotland" => "/gwyliau-banc/yr-alban.ics",
+      "welsh-nothern-ireland" => "/gwyliau-banc/gogledd-iwerddon.ics",
+      "english" => "/bank-holidays/england-and-wales.ics",
+      "english-scotland" => "/bank-holidays/scotland.ics",
+      "english-northern-ireland" => "/bank-holidays/northern-ireland.ics",
+    }
 
-      expected_events = [
-        { "date" => "20120102", "title" => "New Year’s Day" },
-        { "date" => "20120604", "title" => "Spring bank holiday" },
-        { "date" => "20120605", "title" => "Queen’s Diamond Jubilee" },
-        { "date" => "20120827", "title" => "Summer bank holiday" },
-        { "date" => "20121225", "title" => "Christmas Day" },
-        { "date" => "20121226", "title" => "Boxing Day" },
-        { "date" => "20130101", "title" => "New Year’s Day" },
-        { "date" => "20130329", "title" => "Good Friday" },
-        { "date" => "20131225", "title" => "Christmas Day" },
-        { "date" => "20131226", "title" => "Boxing Day" },
-      ]
+    calendars.each do |calendar, calendar_path|
+      should "contain all events in #{calendar}" do
+        get calendar_path
+        assert_equal response.status, 200
 
-      assert(response.body.start_with?("BEGIN:VCALENDAR\r\nVERSION:2.0\r\nMETHOD:PUBLISH\r\nPRODID:-//uk.gov/GOVUK calendars//EN\r\nCALSCALE:GREGORIAN\r\n"))
-      expected_events.each_with_index do |event, _i|
-        expected = ""
-        end_date = (Date.parse(event["date"]) + 1.day).strftime("%Y%m%d")
-        expected << "BEGIN:VEVENT\r\nDTEND;VALUE=DATE:#{end_date}\r\nDTSTART;VALUE=DATE:#{event['date']}\r\nSUMMARY:#{event['title']}\r\n"
-        assert(response.body.include?(expected))
+        expected_non_scottish_events = [
+          { "date" => "20120102", "title" => I18n.t('bank_holidays.new_year') },
+          { "date" => "20120604", "title" => I18n.t('bank_holidays.spring') },
+          { "date" => "20120827", "title" => I18n.t('bank_holidays.summer') },
+        ]
+
+        expected_scottish_events = [
+          { "date" => "20120103", "title" => I18n.t('bank_holidays.new_year') },
+          { "date" => "20120604", "title" => I18n.t('bank_holidays.spring') },
+          { "date" => "20120806", "title" => I18n.t('bank_holidays.summer') },
+        ]
+
+        common_expected_events = [
+          { "date" => "20120605", "title" => I18n.t('bank_holidays.queen_diamond') },
+          { "date" => "20121225", "title" => I18n.t('bank_holidays.christmas') },
+          { "date" => "20121226", "title" => I18n.t('bank_holidays.boxing_day') },
+          { "date" => "20130101", "title" => I18n.t('bank_holidays.new_year') },
+          { "date" => "20130329", "title" => I18n.t('bank_holidays.good_friday') },
+          { "date" => "20131225", "title" => I18n.t('bank_holidays.christmas') },
+          { "date" => "20131226", "title" => I18n.t('bank_holidays.boxing_day') },
+        ]
+
+        expected_events = if calendar.include?("scotland")
+                            expected_scottish_events + common_expected_events
+                          else
+                            expected_non_scottish_events + common_expected_events
+                          end
+
+        assert(
+          response.body.start_with?(
+            "BEGIN:VCALENDAR\r\nVERSION:2.0\r\nMETHOD:PUBLISH\r\nPRODID:-//uk.gov/GOVUK calendars//EN\r\nCALSCALE:GREGORIAN\r\n"
+          )
+        )
+
+        assert_equal "text/calendar", response.content_type
+        assert_equal "max-age=86400, public", response.headers["Cache-Control"]
+
+        expected_events.each do |event|
+          end_date = (Date.parse(event["date"]) + 1.day).strftime("%Y%m%d")
+          expected = "BEGIN:VEVENT\r\nDTEND;VALUE=DATE:#{end_date}\r\nDTSTART;VALUE=DATE:#{event['date']}\r\nSUMMARY:#{event['title']}\r\n"
+
+          assert(response.body.include?(expected))
+        end
       end
-      assert(response.body.end_with?("END:VCALENDAR\r\n"))
-
-      assert_equal "text/calendar", response.content_type
-      assert_equal "max-age=86400, public", response.headers["Cache-Control"]
     end
 
     should "have redirect for old 'ni' division" do
