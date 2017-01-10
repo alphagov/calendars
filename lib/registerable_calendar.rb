18f0f3795c00b2c397c387a4c1a88e04636757c7
Tijmen Brommet <tijmen@gmail.com>
2017-01-10 18:21:27 +0000

    Remove unused method from registerable calendar
    This class used to be used by both panopticon and rummager. Now that
    panopticon is removed as a dependency
    (https://github.com/alphagov/calendars/pull/135) these methods aren't
    necessary anymore.

--- lib/registerable_calendar.rb
+++ lib/registerable_calendar.rb
@@ -3,7 +3,7 @@ require 'ostruct'
 class RegisterableCalendar
   extend Forwardable
 
-  attr_accessor :calendar, :slug, :live
+  attr_accessor :calendar, :slug
 
   def_delegators :@calendar, :indexable_content, :content_id
 
@@ -19,23 +19,5 @@ class RegisterableCalendar
 
   def description
     I18n.t(@calendar.description)
-  end
-
-  def state
-    'live'
-  end
-
-  def need_ids
-    [@calendar.need_id.to_s]
-  end
-
-  # Sending an empty array for `paths` and `prefixes` will make sure we don't
-  # register routes in Panopticon.
-  def paths
-    []
-  end
-
-  def prefixes
-    []
   end
 end
