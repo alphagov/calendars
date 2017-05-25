d397e1a20ea169b143f51c32f309c164afabb3de
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2017-05-25 14:56:56 +0100

    Rails configuration changes after bump to 5.0.3
    These are equivalent to rails app:update changes, minus the irrelevant bits
    like ActiveRecord, ActionCable and Puma config.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,6 @@ class ApplicationController < ActionController::Base
   include Slimmer::Template
   include Slimmer::GovukComponents
 
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   rescue_from GdsApi::TimedOutException, with: :error_503
--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require "action_controller/railtie"
 require "action_mailer/railtie"
@@ -16,16 +16,8 @@ module Calendars
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/lib)
-    config.autoload_paths += %W(#{config.root}/app/services)
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.eager_load_paths += %W(#{config.root}/lib)
+    config.eager_load_paths += %W(#{config.root}/app/services)
 
     # This saves loading all 80-odd locales from the rails-i18n gem.  This doesn't affect the loading
     # of locales from config/locales
