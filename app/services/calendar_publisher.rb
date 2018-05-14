615568f2c1baabe3e3ccbfee4e4cb080200afd9b
Mat Moore <matmoore@users.noreply.github.com>
2018-05-14 15:25:05 +0000

    Set primary publishing organisation
    GDS is the primary publishing organisation for calendars,
    because there is no publishing app for them. They are just
    2 json files on github.

--- app/services/calendar_publisher.rb
+++ app/services/calendar_publisher.rb
@@ -6,6 +6,7 @@ class CalendarPublisher
   def publish
     Services.publishing_api.put_content(rendered.content_id, rendered.payload)
     Services.publishing_api.publish(rendered.content_id)
+    Services.publishing_api.patch_links(rendered.content_id, links: rendered.links)
   end
 
 private
--- test/unit/services/calendar_publisher_test.rb
+++ test/unit/services/calendar_publisher_test.rb
@@ -4,6 +4,7 @@ class CalendarPublisherTest < ActiveSupport::TestCase
   def test_publishing_to_publishing_api
     Services.publishing_api.expects(:put_content).with("58f79dbd-e57f-4ab2-ae96-96df5767d1b2", valid_payload_for('calendar'))
     Services.publishing_api.expects(:publish).with("58f79dbd-e57f-4ab2-ae96-96df5767d1b2")
+    Services.publishing_api.expects(:patch_links).with("58f79dbd-e57f-4ab2-ae96-96df5767d1b2", links: { primary_publishing_organisation: ["af07d5a5-df63-4ddc-9383-6a666845ebe9"] })
 
     calendar = Calendar.find('bank-holidays')
 
