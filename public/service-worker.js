832e753f3895a8dbae042afca1d24e06293c80f0
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2019-05-14 15:38:41 +0100

    Adds client window reload after service worker updates

--- public/service-worker.js
+++ public/service-worker.js
@@ -19,12 +19,24 @@ self.addEventListener('install', function(event) {
   );
 });
 
+self.addEventListener('activate', activateEvent => {
+  // Optional: Get a list of all the current open windows/tabs under
+  // our service worker's control, and force them to reload.
+  // This can "unbreak" any open windows/tabs as soon as the new
+  // service worker activates, rather than users having to manually reload.
+  self.clients.matchAll({ type: 'window' }).then(function(windowClients) {
+    windowClients.forEach(function(windowClient) {
+      windowClient.navigate(windowClient.url);
+    });
+  });
+});
+
 self.addEventListener('fetch', function(event) {
   // request.mode = navigate isn't supported in all browsers
   // so include a check for Accept: text/html header.
   if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
     event.respondWith(
-      fetch(event.request.url).catch(error => {
+      fetch(event.request.url).catch(function(error) {
         // Return the cached page if it exists
         return caches.match(event.request.url);
       })
